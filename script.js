a675dde9f8902b79173c82a9c167dbcb6c00552c
Blessing Chika Onwunali <76081468+Chifrica@users.noreply.github.com>
2024-12-21 18:47:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -100,30 +100,3 @@ document.addEventListener('DOMContentLoaded', () => {
         alert('Report submitted successfully!');
     });
 });
-
-
-
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     const photoPreview = document.getElementById('photoPreview');
-//     const uploadPhotoBtn = document.getElementById('uploadPhotoBtn');
-
-//     let photoFile = null;
-
-//     //upload photo
-
-//     uploadPhotoBtn.addEventListener('click', () => {
-//         const input = document.createElement('input');
-//         input.type = 'file';
-//         input.accept = 'image/*';
-//         input.onchange = (e) => {
-//             photoFile = e.target.files[0]
-//             const img = document.createElement('img');
-//             img.src = URL.createObjectURL(photoFile);
-//             photoPreview.innerHTML = '';
-//             photoPreview.appendChild(img);
-//         }
-//         input.click();
-//     });
-// })
